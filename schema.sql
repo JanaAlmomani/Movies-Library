cd9e07add7f17eb8c28c6fcc80f6dd1e2d6db600
JanaAlmomani <jana2000510@gmail.com>
2023-02-28 14:18:53 +0000

    done

---
+++ schema.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS moviesInfo;
+
+CREATE TABLE IF NOT EXISTS moviesInfo (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255),
+    release_date VARCHAR(255),
+    poster_path VARCHAR(255),
+    overview  VARCHAR(1000)
+);
